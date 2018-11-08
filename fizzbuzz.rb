976717e45452405a97646199a939381e06e2fe43
Kiley Bobbitt <jkileybob@aol.com>
2018-11-08 19:32:45 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(number)
-  if number / 3 == 0
+  if number % 3 == 0
     "Fizz"
-  if number / 5 == 0
+  if number % 5 == 0
     "Buzz"
-  if number / (3 && 5) == 0
+  if (number % 3 ==0) && (number % 5 == 0)
     "FizzBuzz"
-  else number !/ (3 || 5) == 0
+  elsif (number !% 3 == 0) && (number !% 5 == 0)
     nil  
   end  
 end
